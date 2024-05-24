179dbf4c23998ea3e922764c7f7061a293eafb66
girannorbi <girannorbi2007@gmail.com>
2024-05-24 15:47:10 +0200

    a

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -7,7 +7,7 @@ with open('./input.txt', 'r') as f:
     line = "".join(filter(str.isalnum, line)) # filters non-alphanumeric characters from line
     line = line.lower()
     for offset in range(len(line)):
-      if line[offset] != line[len(line)-1 - offset] and isPalindrome: # checks if the carachter from the front is equals to the character in the back, while moving inside
+      if line[offset] != line[len(line)-1 - offset]: # checks if the carachter from the front is equals to the character in the back, while moving inside
         isPalindrome = False
       if line[offset] not in line[0:offset]:
         uniqueCharachters += 1
