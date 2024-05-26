dc3a057daa306fb92b0eed54ae7349cd0436db97
girannorbi <girannorbi2007@gmail.com>
2024-05-26 13:28:11 +0200

    a

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -104,8 +104,8 @@ for i in results:
   for j in i:
     if isinstance(j, str):
       print(j)
-      continue
     else:
+      j.pop(0)
       for row in j:
         print(" ".join(str(number) for number in row))
-    print("")
+      print("")
